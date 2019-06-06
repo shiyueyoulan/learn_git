0fb279cae666c7807f4c363262336c6f2dcd01eb
shiyueyoulan <1141454529@qq.com>
2019-06-06 19:00:53 +0800

    vue学习

---
+++ vue-routerA/router/src/components/Look.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="look">{{message}}
+    {{$route.params.id}}
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Look',
+  data () {
+    return {
+      message: '来看我'
+    }
+  }
+}
+</script>
