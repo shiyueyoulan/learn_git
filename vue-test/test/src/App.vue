9aec30d5d3b0d2e7ae4156a00cb26ddc12a23434
shiyueyoulan <1141454529@qq.com>
2019-06-12 20:42:17 +0800

    vue学习

---
+++ vue-test/test/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="moor-item">
+      <label for>Switch</label>
+      <moor-switch v-model="ligthSwitch">开关(开);</moor-switch>
+    </div>
+  </div>
+</template>
+
+<script>
+import MoorSwitch from "./components/MoorSwitch";
+export default {
+  name: "app",
+  data() {
+    return {
+      ligthSwitch: false
+    };
+  },
+  components: {
+    "moor-switch": MoorSwitch
+  }
+};
+</script>
+
+<style>
+</style>
